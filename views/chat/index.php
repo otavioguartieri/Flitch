6c53f48a03c7f29f8f82bc953de2ecee5b6cfb30
otavioguartieri <otaviorg10@gmail.com>
2023-04-30 01:06:39 +0000

    chat base pronto

--- views/chat/index.php
+++ views/chat/index.php
@@ -1,1 +1,11 @@
-chat
+<div class="message-body">
+    <div class="message-content"></div>
+    <div class="message-send">
+        <div class="message-area-text">
+            <textarea name="message_value" id="message_value"></textarea>
+        </div>
+        <div class="message-area-button">
+            <div class="message_button"><i class="fa-solid fa-paper-plane"></i></div>
+        </div>
+    </div>
+</div>
